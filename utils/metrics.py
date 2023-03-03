31b31d8bf43ca5f8f174c7a2144adadc5abe2050
LiaoMengqi <1799859708@qq.com>
2023-03-03 16:52:38 +0800

    metrics

---
+++ utils/metrics.py
@@ -0,0 +1,14 @@
+import numpy as np
+from scipy.stats import rankdata
+
+
+def calculate_rank(scores: np.ndarray, target_index: np.ndarray):
+    return rankdata(-scores, axis=-1)[np.arange(target_index.shape[0]), target_index]
+
+
+def calculate_hist(k: int, ranks: np.ndarray):
+    return np.sum(ranks <= k) / len(ranks)
+
+
+def calculate_mrr(ranks: np.ndarray):
+    return (1. / ranks).sum() / ranks.shape[0]
